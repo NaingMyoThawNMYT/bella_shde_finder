6e032d89d0161906ebc866f8df45520ac7d59009
NaingMyoThawNMYT <naingmyothaw.nmyt@gmail.com>
2019-10-24 22:59:31 +0630

    Add Product List Activity and Order List Activity.

--- app/src/main/java/com/example/bellashdefinder/activity/AdminActivity.java
+++ app/src/main/java/com/example/bellashdefinder/activity/AdminActivity.java
@@ -1,6 +1,8 @@
 package com.example.bellashdefinder.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +14,13 @@ public class AdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+    }
+
+    public void goToProductList(View v) {
+        startActivity(new Intent(this, ProductListActivity.class));
+    }
+
+    public void goToOrderList(View v) {
+        startActivity(new Intent(this, OrderListActivity.class));
     }
 }
