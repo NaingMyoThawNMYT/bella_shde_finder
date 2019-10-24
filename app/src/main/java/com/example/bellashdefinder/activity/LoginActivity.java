9ac4350be5a05087f00ac1a69bee8d8879882c15
NaingMyoThawNMYT <naingmyothaw.nmyt@gmail.com>
2019-10-24 22:43:04 +0630

    Add Admin Activity and Customer Activity.

--- app/src/main/java/com/example/bellashdefinder/activity/LoginActivity.java
+++ app/src/main/java/com/example/bellashdefinder/activity/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.bellashdefinder.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -9,6 +10,7 @@ import com.example.bellashdefinder.R;
 
 public class LoginActivity extends AppCompatActivity {
     public static final String KEY_ADMIN_MODE = "admin_mode";
+    private boolean adminMode = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +19,26 @@ public class LoginActivity extends AppCompatActivity {
 
         Bundle b = getIntent().getExtras();
         if (b != null && b.containsKey(KEY_ADMIN_MODE)) {
-            if (!b.getBoolean(KEY_ADMIN_MODE)) {
+            adminMode = b.getBoolean(KEY_ADMIN_MODE);
+            if (!adminMode) {
                 findViewById(R.id.btn_skip_login).setVisibility(View.VISIBLE);
             }
         }
+    }
+
+    public void login(View v) {
+        if (adminMode) {
+            goToAdminActivity(v);
+        } else {
+            goToCustomerActivity(v);
+        }
+    }
+
+    public void goToAdminActivity(View v) {
+        startActivity(new Intent(this, AdminActivity.class));
+    }
+
+    public void goToCustomerActivity(View v) {
+        startActivity(new Intent(this, CustomerActivity.class));
     }
 }
